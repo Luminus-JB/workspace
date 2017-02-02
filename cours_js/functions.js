3ddd1e60ae9c8d70964938f94e5b97e3243fdf49
Luminus-JB <pugjub@gmail.com>
2017-02-02 08:24:06 +0100

    Recommencement du projet homepage - Ajout du repo cours_html et cours_js

---
+++ cours_js/functions.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Julien on 01/02/2017.
+ */
+/* fonction permettant de changer un texte selon l'image selectionner */
+function affiche_text(text){
+    document.getElementById("2").innerHTML = text;
+}
